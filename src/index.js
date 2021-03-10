586a700f9774483120e44bb956e86ffd43547aff
MilaKolasava <kolasava.dev@yandex.by>
2021-03-10 19:25:54 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+   n = String(Math.abs(n));
+    let result = n.split('').reverse().join('');
+    return result;
 }
